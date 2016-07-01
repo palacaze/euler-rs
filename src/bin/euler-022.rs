50fc193e09d9fed59cb13380e5af564a74f72d2f
Pierre-Antoine Lacaze <pa.lacaze@laposte.net>
2016-07-01 16:22:26 +0200

    solution to problem 022

---
+++ src/bin/euler-022.rs
@@ -0,0 +1,42 @@
+// Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names,
+// begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value
+// by its alphabetical position in the list to obtain a name score.
+//
+// For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53,
+// is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+//
+// What is the total of all the name scores in the file?
+
+use std::env;
+use std::io::Read;
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+
+fn count_chars(s : &String) -> u32 {
+    s.chars().map(|c| c as u32 - 64).fold(0, |a, c| a+c)
+}
+
+fn main() {
+    let path: String = env::args().nth(1).expect("Must supply a file name");
+    
+    let path = Path::new(&path);
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+
+    let mut names = String::new();
+    match file.read_to_string(&mut names) {
+        Err(why) => panic!("couldn't read {}: {}", display, why.description()),
+        Ok(_) => {},
+    }
+
+    let mut v : Vec<String> = names.split(',').map(|s| s.trim_matches('"').to_string()).collect();
+    v.sort();
+    let r = v.iter().map(count_chars).enumerate().fold(0, |a, (i, n)| a + (i+1)*(n as usize));
+
+    println!("sum = {:?}", r);
+}
