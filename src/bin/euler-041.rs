2c70ad749efbc0c3b82467193065f612902678bb
Pierre-Antoine Lacaze <pa.lacaze@laposte.net>
2016-07-04 22:20:55 +0200

    update solution to problem 41
    * The cache is not worth it
    * I realized some numbers 8 and 9 digits don't need to be prospected.
    * There was an indexing problem, as an happy accident my error, skipped 8 and 9 digits

--- src/bin/euler-041.rs
+++ src/bin/euler-041.rs
@@ -5,14 +5,12 @@
 //
 // What is the largest n-digit pandigital prime that exists?
 
+// no need to try 8 or 9 digits, because divisibility rules show those are divisible by 3
 // The best seems to be going backward from 987654321, we will test all digits permutations
-// for prime.
-// In order to speed-up the process we will first generate all the primes < sqrt(987654321).
+// for primes
 
 #[macro_use]
 extern crate itertools;
-
-use std::collections::HashSet;
 use itertools::Itertools;
 
 
@@ -27,25 +25,6 @@ fn is_prime(n : &usize) -> bool {
         }
     }
     true
-}
-
-// determine if n is prime using a cache to speed up the computation
-// here we assume that cache contains all the primes up to sqrt(n)
-fn is_prime_with_cache(n : &usize, cache: &HashSet<usize>) -> bool {
-    for x in cache {
-        if *n % x == 0 {
-            return false;
-        }
-    }
-    true
-}
-
-// create a cache of prime numbers that can be used to test up to max_n
-fn primes_cache(max_n: usize) -> HashSet<usize> {
-    let lim = (max_n as f64).sqrt() as usize + 1;
-    let mut cache = (3..lim).step(2).filter(is_prime).collect::<HashSet<_>>();
-    cache.insert(2);
-    cache
 }
 
 // Generate all the numbers through digits permutation from supplied list of digits.
@@ -73,14 +52,13 @@ fn digit_permutations(digit_set: &[usize]) -> Vec<usize> {
 
 fn main() {
     // we test from biggest number of digits down
-    for i in (2..10).rev() {
-        let cache = primes_cache(10usize.pow(i as u32));
+    for i in (2..8).rev() {
         // we create a reversed ordered list of digits in order to iterate
         // permutations also from the biggest down.
         // That way we know that the first encountered prime will be our answer
-        let digits = (1..(i-1)).rev().collect::<Vec<_>>();
+        let digits = (1..(i+1)).rev().collect::<Vec<_>>();
         for p in digit_permutations(&digits) {
-            if is_prime_with_cache(&p, &cache) {
+            if is_prime(&p) {
                 println!("max = {:?}", p);
                 return;
             }
