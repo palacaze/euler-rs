0bdc88d3ce4c57ef20014de6c57e91721d6a42f6
Pierre-Antoine Lacaze <pa.lacaze@laposte.net>
2016-07-19 09:33:22 +0200

    improve solution to problem 060
    - no need to use digit arrays, an appropriate multiplier does the job
    - enforce ordering of primes p1 < p2 < p3 < p4 < p5: less work
    
    It now runs 3 times as fast.

--- src/bin/euler-060.rs
+++ src/bin/euler-060.rs
@@ -13,53 +13,54 @@ extern crate test;
 
 extern crate primal;
 
-extern crate euler;
-use euler::int::{Digits};
-
 #[derive(Debug, Clone)]
 struct DPrime {
     n: usize,
-    d: Vec<u8>,
+    fac: usize,
 }
 
 impl DPrime {
     fn new(p: usize) -> Self {
-        DPrime { n: p, d: p.to_digits() }
+        DPrime { n: p, fac: next_pow10(p) }
     }
 }
 
-fn are_concat_primes(a: &DPrime, b: &DPrime, sieve: &primal::Sieve) -> bool {
-    let n = a.d.iter().rev().chain(b.d.iter().rev()).fold(0, |a, &d| 10 * a + d as usize);
-    if !sieve.is_prime(n) {
-        return false;
-    }
-    let n = b.d.iter().rev().chain(a.d.iter().rev()).fold(0, |a, &d| 10 * a + d as usize);
-    sieve.is_prime(n)
+fn next_pow10(n: usize) -> usize {
+    let mut p = 10;
+    while n > p { p *= 10; }
+    p
 }
 
 pub fn solve() -> (usize, [usize; 5]) {
+    let lim = 10_000;
+
     // primes under 10_000 except for 2 and 5 that can't work
     let mut primes = vec![DPrime::new(3)];
-    primes.extend(primal::Primes::all().skip(3).take_while(|&n| n < 10_000).map(DPrime::new));
+    primes.extend(primal::Primes::all().skip(3).take_while(|&n| n < lim).map(DPrime::new));
 
-    // a cache of primes for faster lookup
-    let sieve = primal::Sieve::new(100_000_000);
+    // a cache of primes for faster lookup and function to test
+    // concatenated primes using it
+    let sieve = primal::Sieve::new(lim * lim);
+    let are_coprimes = |a: &DPrime, b: &DPrime| {
+        b.n > a.n && sieve.is_prime(a.n + a.fac * b.n) &&
+                     sieve.is_prime(b.n + b.fac * a.n)
+    };
 
     let mut best_sum = 1_000_000_000_000;
     let mut best_tup = [0; 5];
 
     for p1 in &primes {
         if p1.n >= best_sum { break; }
-        let p2_list = primes.iter().filter(|p2| are_concat_primes(&p1, p2, &sieve)).collect::<Vec<_>>();
-        for p2 in &p2_list {
+        let p2l = primes.iter().filter(|p2| are_coprimes(&p1, p2)).collect::<Vec<_>>();
+        for p2 in &p2l {
             if p1.n + p2.n >= best_sum { break; }
-            let p3_list = p2_list.iter().filter(|p3| are_concat_primes(p2, p3, &sieve)).collect::<Vec<_>>();
-            for p3 in &p3_list {
+            let p3l = p2l.iter().filter(|p3| are_coprimes(p2, p3)).collect::<Vec<_>>();
+            for p3 in &p3l {
                 if p1.n + p2.n + p3.n >= best_sum { break; }
-                let p4_list = p3_list.iter().filter(|p4| are_concat_primes(p3, p4, &sieve)).collect::<Vec<_>>();
-                for p4 in &p4_list {
+                let p4l = p3l.iter().filter(|p4| are_coprimes(p3, p4)).collect::<Vec<_>>();
+                for p4 in &p4l {
                     if p1.n + p2.n + p3.n + p4.n >= best_sum { break; }
-                    if let Some(p5) = p4_list.iter().filter(|p5| are_concat_primes(p4, p5, &sieve)).nth(0) {
+                    if let Some(p5) = p4l.iter().filter(|p5| are_coprimes(p4, p5)).nth(0) {
                         let sum = p1.n + p2.n + p3.n + p4.n + p5.n;
                         if sum < best_sum {
                             best_sum = sum;
