f31f0ce91b6ec2c8d007f7e3a97c0c643779a597
Pierre-Antoine Lacaze <pa.lacaze@laposte.net>
2016-08-02 15:12:52 +0200

    second solution to problem 079

--- src/bin/euler-079.rs
+++ src/bin/euler-079.rs
@@ -13,32 +13,60 @@
 extern crate test;
 extern crate permutohedron;
 extern crate time;
+extern crate petgraph;
 
 use permutohedron::Heap;
 use time::PreciseTime;
+use petgraph::Graph;
 
 use std::env;
 use std::io::Read;
 use std::fs::File;
 use std::path::Path;
+use std::collections::HashMap;
 
-fn read_data(path: &str) -> Vec<Vec<u8>> {
+fn read_data(path: &str) -> Vec<Vec<char>> {
     let path = Path::new(&path);
     let mut file = File::open(&path).unwrap();
     let mut data = String::new();
     let _ = file.read_to_string(&mut data).unwrap();
     let mut v = data
         .lines()
-        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap() as u8)
-                  .collect::<Vec<_>>())
+        .map(|l| l.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
     v.sort();
     v.dedup();
     v
 }
 
-fn match_order(sample: &[u8], key: &[u8]) -> bool {
+pub fn solve_graph(path: &str) -> String {
+    let logs = read_data(path);
 
+    // unique chars
+    let mut chars = logs.iter().flat_map(|s| s.clone()).collect::<Vec<_>>();
+    chars.sort();
+    chars.dedup();
+
+    // build a graph with unique chars as nodes
+    let mut g = Graph::<char, char>::new();
+    let nodes = chars.iter().map(|&c| (c, g.add_node(c))).collect::<HashMap<_, _>>();
+
+    // build a list of directed edges
+    let mut edges = logs.iter()
+        .flat_map(|l| vec![(*nodes.get(&l[0]).unwrap(), *nodes.get(&l[1]).unwrap()),
+                           (*nodes.get(&l[1]).unwrap(), *nodes.get(&l[2]).unwrap())])
+        .collect::<Vec<_>>();
+    edges.sort();
+    edges.dedup();
+
+    // set edges and apply topological sorting to the graph
+    g.extend_with_edges(&edges);
+    petgraph::algo::toposort(&g).iter()
+        .map(|n| *g.node_weight(*n).unwrap())
+        .collect()
+}
+
+fn match_order(sample: &[char], key: &[char]) -> bool {
     let mut k = 0;
     for &e in sample {
         while k < key.len() && key[k] != e {
@@ -48,7 +76,6 @@ fn match_order(sample: &[u8], key: &[u8]) -> bool {
             return false;
         }
     }
-
     true
 }
 
@@ -63,7 +90,7 @@ pub fn solve(path: &str) -> String {
             if !match_order(&log, &key) { continue 'next_key; }
         }
 
-        return key.iter().map(|c| std::char::from_digit(*c as u32, 10).unwrap()).collect();
+        return key.into_iter().collect();
     }
 
     String::new()
@@ -71,9 +98,14 @@ pub fn solve(path: &str) -> String {
 
 fn main() {
     let path: String = env::args().nth(1).expect("Must supply a file name");
+
     let start = PreciseTime::now();
     let s = solve(&path);
-    println!("assuming unique digits: {} ({})", s, start.to(PreciseTime::now()));
+    println!("password: {} ({})", s, start.to(PreciseTime::now()));
+
+    let start = PreciseTime::now();
+    let s = solve_graph(&path);
+    println!("password (graph): {} ({})", s, start.to(PreciseTime::now()));
 }
 
 #[cfg(test)]
@@ -82,16 +114,29 @@ mod tests {
     use test::{Bencher, black_box};
 
     #[test]
-    fn test_67() {
+    fn test_79() {
         let path = "data/p079_keylog.txt";
         let s = solve(&path);
         assert_eq!("73162890", s);
     }
 
+    #[test]
+    fn test_graph_79() {
+        let path = "data/p079_keylog.txt";
+        let s = solve_graph(&path);
+        assert_eq!("73162890", s);
+    }
+
     #[bench]
-    fn bench_67(b: &mut Bencher) {
+    fn bench_79(b: &mut Bencher) {
         let path = "data/p079_keylog.txt";
         b.iter(|| black_box(solve(&path)));
+    }
+
+    #[bench]
+    fn bench_graph_79(b: &mut Bencher) {
+        let path = "data/p079_keylog.txt";
+        b.iter(|| black_box(solve_graph(&path)));
     }
 }
 
